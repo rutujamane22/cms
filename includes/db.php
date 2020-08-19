ceb6c7d82ac4f75773f4f2a7c2938dc83babccd1
rutujamane22 <rutujam2208@gmail.com>
2020-08-19 21:12:40 +0530

    added remote mysql connection details

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,15 @@
 <?php
+// development connwction
+// $db['host']="localhost";
+// $db['user']="root";
+// $db['pw']="";
+// $db['name']="cms";
 
-$db['host']="localhost";
-$db['user']="root";
-$db['pw']="";
-$db['name']="cms";
+//remote database connection
+$db['host']="remotemysql.com";
+$db['user']="ol9lbRxuVZ";
+$db['pw']="yeFagUeV4r";
+$db['name']="ol9lbRxuVZ";
 
 foreach($db as $key => $value){
   define(strtoupper($key),$value);
